77e5a4d901e2dadf4c615c94a5a8b4ec6a13f682
Timofey Pchelintsev <tpchelintsev@gmail.com>
2018-07-30 16:46:21 +0300

    Update hi.view.js

--- hi.view.js
+++ hi.view.js
@@ -1,2 +1,2 @@
 const {node} = props
-return e(Box, {'background': 'neutral-2', align: 'center', justify: 'center', fill: true}, `Hi, ${node.name}!`)
+return e(Box, {'background': 'neutral-3', align: 'center', justify: 'center', fill: true}, `Hi, ${node.name}!`)
